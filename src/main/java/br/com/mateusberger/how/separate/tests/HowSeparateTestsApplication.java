12fa5359c705f1119c99820aa7a1bbe7d0ae66de
Mateus Berger <mateus.berger@compasso.com.br>
2023-10-26 16:58:36 -0300

    Inicial Commit

---
+++ src/main/java/br/com/mateusberger/how/separate/tests/HowSeparateTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateusberger.how.separate.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowSeparateTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowSeparateTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mateusberger/how/separate/tests/HowSeparateTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusberger.how.separate.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowSeparateTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
